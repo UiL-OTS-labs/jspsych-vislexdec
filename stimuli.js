252f830fec0d3919ee2009041e2110937acadeab
Jacco van Elst <j.c.vanelst@uu.nl>
2021-01-31 19:26:58 +0100

    restyle visual basic

--- stimuli.js
+++ stimuli.js
@@ -1,61 +1,57 @@
+////////////////
+// STIMULI
+///////////////
+
 // Item types
 const NON_WORD = "NON_WORD";
 const REAL_WORD = "REAL_WORD";
+const PRACTICE = "PRACTICE";
 
-const GROUPS = [
-    "group1"
-    // "group2",
-    // "group3"
+const LISTS = [
+    "my_one_and_only_list"
 ];
+
+// In case of more complex design, the above could be, for example:
+
+// const LISTS = [
+//     "my_first_list",
+//     "my_second_list"
+// ];
 
 const PRACTICE_ITEMS = [
-    { id: 1, item_type: NON_WORD, word: "palve" },
-    { id: 2, item_type: REAL_WORD, word: "hot" }
+    { id: 1, item_type: PRACTICE, word: "palve", correct: 0 },
+    { id: 2, item_type: PRACTICE, word: "hot", correct: 1 }
 ];
 
-const LIST_GROUP1 = [
-    { id: 1, item_type: NON_WORD, word: "slirque" },
-    { id: 2, item_type: NON_WORD, word: "crawse" },
-    { id: 3, item_type: NON_WORD, word: "thwurp" },
-    { id: 4, item_type: NON_WORD, word: "clem" },
-    { id: 5, item_type: REAL_WORD, word: "white" },
-    { id: 6, item_type: REAL_WORD, word: "travel" },
-    { id: 7, item_type: REAL_WORD, word: "letter" },
-    { id: 8, item_type: REAL_WORD, word: "clown" }
+const LIST_1 = [
+    { id: 1, item_type: NON_WORD, word: "slirque", correct: 0 },
+    { id: 2, item_type: NON_WORD, word: "crawse", correct: 0 },
+    { id: 3, item_type: NON_WORD, word: "thwurp", correct: 0 },
+    { id: 4, item_type: NON_WORD, word: "clem", correct: 0},
+    { id: 5, item_type: REAL_WORD, word: "white", correct: 1 },
+    { id: 6, item_type: REAL_WORD, word: "travel", correct: 1 },
+    { id: 7, item_type: REAL_WORD, word: "letter", correct: 1 },
+    { id: 8, item_type: REAL_WORD, word: "clown", correct: 1 }
 ];
 
-// Add a second list of stimuli when required.
-// const LIST_GROUP2 = [
-// ...
-// ]
 
 const TEST_ITEMS = [
-    {group_name: GROUPS[0], table: LIST_GROUP1}
-    // Add the second group here, put a comma on the end of the line above here.
-    //{group_name: GROUPS[1], table: LIST_GROUP2}
+    {list_name: LISTS[0], table: LIST_1}
 ];
 
-/**
- * Get the list of practice items
- *
- * Returns an object with a group and a table, the group will always indicate
- * "practice" since it are the practice items
- *
- * @returns {object} object with group and table fields
- */
+// If there were two lists to choose from:
+
+// const TEST_ITEMS = [
+//     {list_name: LISTS[0], table: LIST_1},
+//     {list_name: LISTS[1], table: LIST_2}
+// ];
+
+
 function getPracticeItems() {
-    return {group_name : "practice", table : PRACTICE_ITEMS};
+    return {list_name : "practice", table : PRACTICE_ITEMS};
 }
 
-/**
- * This function will pick a random group from the TEST_ITEMS array.
- *
- * Returns an object with a group and a table, the group will always indicate
- * which list has been chosen for the participant.
- *
- * @returns {object} object with group and table fields
- */
-function pickRandomGroup() {
+function pickRandomList() {
     let range = function (n) {
         let empty_array = [];
         let i;
@@ -64,8 +60,8 @@ function pickRandomGroup() {
         }
         return empty_array;
     }
-    let num_groups = TEST_ITEMS.length;
-    var shuffled_range = jsPsych.randomization.repeat(range(num_groups), 1)
-    var retgroup = TEST_ITEMS[shuffled_range[0]];
-    return retgroup
+    let num_lists = TEST_ITEMS.length;
+    var shuffled_range = jsPsych.randomization.repeat(range(num_lists), 1)
+    var retlist = TEST_ITEMS[shuffled_range[0]];
+    return retlist
 }
